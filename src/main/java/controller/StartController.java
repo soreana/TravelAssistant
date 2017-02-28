e90dd7fc5cf59e514d0f9fa46dadd540fa81ce34
Sina Kashi Pazha <esterlinkof@gmail.com>
2017-03-01 00:17:31 +0330

    send welcome message.

---
+++ src/main/java/controller/StartController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import helper.Messages;
+import helper.TelegramMessages;
+
+/**
+ * Created by sinakashipazha on 2/28/2017 AD.
+ */
+public class StartController extends Controller {
+
+    @Override
+    public void incomingMessage(String message) {
+        // todo save new user account
+        TelegramMessages.sendMessageToUser(TelegramMessages.getChatId(message),
+                Messages.getWelcomeMessage());
+        // todo set key board for travel
+    }
+}
