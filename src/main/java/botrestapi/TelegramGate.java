43e420f9519521808e44cfeab85829db77d605d5
Sina Kashi Pazha <esterlinkof@gmail.com>
2017-02-28 15:21:36 +0330

    create telegram gate as api for telegram.

---
+++ src/main/java/botrestapi/TelegramGate.java
@@ -0,0 +1,51 @@
+package botrestapi;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+
+/**
+ * Created by sinakashipazha on 2/27/2017 AD.
+ */
+
+public class TelegramGate {
+
+    private static int portNumber ;
+    private static String url = Token.getToken();
+
+    public static void main (String [] args) {
+        portNumber = Integer.parseInt(args[0]);
+
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(portNumber), 0);
+            server.createContext(url, new MyHandler());
+            server.setExecutor(null); // creates a default executor
+            server.start();
+        } catch (IOException f) {
+            f.printStackTrace();
+        }
+    }
+
+    static class MyHandler implements HttpHandler{
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+
+            String response = "hello world";
+            InputStream temp = t.getRequestBody();
+            BufferedReader in= new BufferedReader(new InputStreamReader(temp));
+            String input = "",temp2 = "";
+            while ((temp2 = in.readLine() ) != null ){
+                input += temp2;
+            }
+
+            t.sendResponseHeaders(200, response.length());
+            System.out.println(response);
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
