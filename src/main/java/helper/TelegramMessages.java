85edcf65cd70965e8462e0a161166ebede051a84
Sina Kashi Pazha <esterlinkof@gmail.com>
2017-02-28 23:20:12 +0330

    implement getChatId method.

--- src/main/java/helper/TelegramMessages.java
+++ src/main/java/helper/TelegramMessages.java
@@ -1,10 +1,19 @@
 package helper;
 
+import org.json.JSONObject;
+
 /**
  * Created by sinakashipazha on 2/28/2017 AD.
  */
 public class TelegramMessages {
-    public static void sendMessageToUser(String chatId,String message){
+    public static void sendMessageToUser(String chatId, String message) {
 
+    }
+
+    public static String getChatId(String message) {
+        JSONObject jsonObject = new JSONObject(message);
+        jsonObject = jsonObject.getJSONObject("message");
+        jsonObject = jsonObject.getJSONObject("chat");
+        return jsonObject.getString("id");
     }
 }
