3c3869dfd727c82bcf8944f8966a42d440a6c319
Sina Kashi Pazha <esterlinkof@gmail.com>
2017-03-01 00:25:19 +0330

    finalize Controller class.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -3,9 +3,8 @@ package controller;
 /**
  * Created by sinakashipazha on 2/28/2017 AD.
  */
+
 abstract public class Controller {
 
-
-
-    public abstract Controller controllerFactory(String controller, String message);
+    public abstract void incomingMessage(String message);
 }
