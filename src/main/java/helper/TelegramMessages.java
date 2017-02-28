23b09864019d65739c98ff55e30373c2939ea37b
Sina Kashi Pazha <esterlinkof@gmail.com>
2017-02-28 23:14:03 +0330

    create helper package to put frequently used method in one place.

---
+++ src/main/java/helper/TelegramMessages.java
@@ -0,0 +1,10 @@
+package helper;
+
+/**
+ * Created by sinakashipazha on 2/28/2017 AD.
+ */
+public class TelegramMessages {
+    public static void sendMessageToUser(String chatId,String message){
+
+    }
+}
