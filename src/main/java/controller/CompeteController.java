de7a6f381257fcbf2207c065768a888212993182
Sina Kashi Pazha <esterlinkof@gmail.com>
2017-03-01 08:07:11 +0330

    put empty function to CompeteController.

---
+++ src/main/java/controller/CompeteController.java
@@ -0,0 +1,11 @@
+package controller;
+
+/**
+ * Created by sinakashipazha on 2/28/2017 AD.
+ */
+public class CompeteController extends Controller {
+    @Override
+    public void incomingMessage(String message) {
+
+    }
+}
