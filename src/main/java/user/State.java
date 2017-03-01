54b62f8ccf94e7bfe8751ef93b34eb65a00b3a87
Sina Kashi Pazha <esterlinkof@gmail.com>
2017-03-01 11:48:27 +0330

    define user states.

--- src/main/java/user/State.java
+++
@@ -1,9 +0,0 @@
-package user;
-
-/**
- * Created by sinakashipazha on 3/1/2017 AD.
- */
-public enum State {
-    CHOOOSE_ORIGIN,
-    NOTHING;
-}
---
+++ src/main/java/user/UserState.java
@@ -0,0 +1,15 @@
+package user;
+
+/**
+ * Created by sinakashipazha on 3/1/2017 AD.
+ */
+public enum UserState {
+    NOTHING,
+    CHOSEN_ORIGIN,
+    CHOSEN_DESTINATION,
+    CHOSEN_YEAR,
+    CHOSEN_MONTH,
+    CHOSEN_DAY,
+    CHOSEN_DURATION,
+    CHOSEN_TRAVEL_TYPE;
+}
