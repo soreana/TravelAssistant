1ce44d8e41279dbb2453c249480a5cef1b6591af
Sina Kashi Pazha <esterlinkof@gmail.com>
2017-03-03 01:30:27 +0330

    save new User to app.

--- src/main/java/controller/StartController.java
+++ src/main/java/controller/StartController.java
@@ -2,15 +2,23 @@ package controller;
 
 import helper.Messages;
 import helper.TelegramMessages;
+import org.json.JSONObject;
+import user.UserManager;
 
 /**
  * Created by sinakashipazha on 2/28/2017 AD.
  */
 public class StartController extends Controller {
 
+    private JSONObject getFromPartOfMessage(String message){
+        return new JSONObject(message)
+                .getJSONObject("message")
+                .getJSONObject("from");
+    }
+
     @Override
     public void incomingMessage(String message) {
-        // todo save new user account
+        UserManager.putUser(getFromPartOfMessage(message));
         TelegramMessages.sendMessageToUser(TelegramMessages.getChatId(message),
                 Messages.getWelcomeMessage());
         TelegramMessages.travelOrCompeteKeyboardToChat(TelegramMessages.getChatId(message));
