7da072e118ab6d0eadbf7635c1fd7139a6fd07cf
Sina Kashi Pazha <esterlinkof@gmail.com>
2017-03-03 01:38:45 +0330

    create constructor for class User.

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,20 +1,39 @@
 package user;
 
+import org.json.JSONObject;
+
 /**
  * Created by sinakashipazha on 2/27/2017 AD.
  */
 public class User {
-    private String userTelegramID;
+    private String telegramID;
+    private String firstName;
+    private String lastName;
+    private String userName;
+
     private int score;
-    private String userToken;
     private UserState userState;
+
+    private String userToken;
+
+    public User(JSONObject jsonObject){
+        telegramID = jsonObject.getString("id");
+        firstName = jsonObject.getString("first_name");
+        lastName = jsonObject.getString("last_name");
+        userName = jsonObject.getString("username");
+
+        score = 0;
+        userState = UserState.NOTHING;
+
+        // todo generate user token
+    }
 
     public UserState getState() {
         return userState;
     }
 
-    public String getUserTelegramID() {
-        return userTelegramID;
+    public String getTelegramID() {
+        return telegramID;
     }
 
     public void changeStateForward() {
