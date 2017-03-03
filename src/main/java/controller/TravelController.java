6269e2fc3dd84e89b7bb77caa6abab58006a13e9
Sina Kashi Pazha <esterlinkof@gmail.com>
2017-03-04 00:17:58 +0330

    refactor class TravelController.

--- src/main/java/controller/TravelController.java
+++ src/main/java/controller/TravelController.java
@@ -21,7 +21,6 @@ public class TravelController extends Controller {
 
         switch (state) {
             case SENT_ORIGIN:
-                System.out.println("sent origin.");
                 origin = TelegramMessages.getTextPartOfMessage(message).substring(7);
                 UserManager.getUserById(userId)
                         .instantiateNewTravel(origin)
@@ -49,7 +48,6 @@ public class TravelController extends Controller {
                 UserManager.getUserById(userId).changeStateForward();
                 break;
             case CHOSEN_ORIGIN:
-                System.out.println("chosen_origin");
                 TelegramMessages.sendDestinationsListToUser(chatId,
                         TicketAPI.getDestinationForThisOrigin(origin));
                 UserManager.getUserById(userId)
